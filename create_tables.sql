4e0cb8755017471264162454ef9b28d03b547cfc
Doma <100135188+DomaGamerRDG@users.noreply.github.com>
2026-02-25 10:06:03 +0100

    add password_token table

--- create_tables.sql
+++ create_tables.sql
@@ -298,7 +298,23 @@ CREATE TABLE IF NOT EXISTS notifications (
     ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+-- -------------------------
+-- 10) PASSWORD_TOKEN
+-- -------------------------
+CREATE TABLE password_tokens (
+  id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 
+  user_id BIGINT UNSIGNED NOT NULL,
 
+  token VARCHAR(128) NOT NULL,
+  expires_at DATETIME NOT NULL,
 
+  used TINYINT(1) NOT NULL DEFAULT 0,
 
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+  CONSTRAINT fk_pt_user
+    FOREIGN KEY (user_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE
+);
